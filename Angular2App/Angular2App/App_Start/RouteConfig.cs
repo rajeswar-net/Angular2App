c785d9a19376a676adeadf774518a45740d89bef
rmandli <rajeswar.net@gmail.com>
2018-05-17 13:55:47 -0500

    Initial working project Checkin

--- Angular2App/Angular2App/App_Start/RouteConfig.cs
+++ Angular2App/Angular2App/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace Angular2App
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{*anything}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
